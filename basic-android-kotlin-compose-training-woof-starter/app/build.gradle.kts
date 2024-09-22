ae4d451a98a355b8b068bd21533db9b927acb2ba
Audrey-Sanchez <124395245+Sherlock-AS@users.noreply.github.com>
2024-09-22 17:41:45 -0400

    Exo 2 etape 4

--- basic-android-kotlin-compose-training-woof-starter/app/build.gradle.kts
+++ basic-android-kotlin-compose-training-woof-starter/app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.wear.compose:compose-material3:1.0.0-alpha25")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
